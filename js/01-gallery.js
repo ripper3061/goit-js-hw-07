29f59b12011030cfe397c3ca111cad563c88082f
Vladimir Ponomarov <ripper3061@gmail.com>
2022-08-11 13:29:19 +0300

    рефакторинг кода

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,52 +1,50 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
-
 const makeGalleryCardsMakeup = ({ preview, original, description }) => {
   return `
   <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
+  <img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+  />
   </a>
-`;
+  `;
 };
-
 const makeGalleryList = galleryItems.map(makeGalleryCardsMakeup).join("");
-
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", onOpenModal);
 gallery.insertAdjacentHTML("beforeend", makeGalleryList);
+window.addEventListener("keydown", onCloseModal);
 
-gallery.addEventListener("click", onModalOnClick);
 let modal;
-function onModalOnClick(event) {
+function onOpenModal(event) {
   event.preventDefault();
   if (!event.target.classList.contains("gallery__image")) {
     return;
   }
   const imageOriginalLink = event.target.dataset.source;
-
-  // openModal
-  openModal(imageOriginalLink);
-}
-function openModal(imageOriginalLink) {
   modal = basicLightbox.create(
-    `<img width="1400" height="900" src="${imageOriginalLink}"> `,
-    {
-      onShow: (modal) => {
-        window.addEventListener("keydown", () => {
-          if (event.code === "Escape") {
-            modal.close();
-          }
-        });
-      },
-    }
+    `<img width="1400" height="900" src="${imageOriginalLink}"> `
   );
   modal.show();
 }
-// const modal = openModal(imageOriginalLink);
 
-// window.addEventListener("keydown", openModal(imageOriginalLink));
+function onCloseModal(event) {
+  if (event.code === "Escape") {
+    modal.close();
+  }
+}
+
+// ,
+//     {
+//       onShow: (modal) => {
+//         window.addEventListener("keydown", () => {
+//           if (event.code === "Escape") {
+//             modal.close();
+//           }
+//         });
+//       },
+//     }
