2eb72511880f1851b56026acc75fa8eb28284d65
Vladimir Ponomarov <ripper3061@gmail.com>
2022-08-12 10:20:02 +0300

    2 Task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const makeGalleryCard = ({ preview, original, description }) => {
+  return `
+  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+  `;
+};
+
+const makeGalleryCardList = galleryItems.map(makeGalleryCard).join("");
+gallery.insertAdjacentHTML("beforeend", makeGalleryCardList);
+
+gallery.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+}
+
+let galleryS = new SimpleLightbox(".gallery .gallery__item", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+galleryS.on("shown.simplelightbox");
